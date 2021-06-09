1d94bb5976ab22836b9286c2c9816cbccf184b0f
Jace <bellcastle88@gmail.com>
2021-06-09 21:38:29 +0900

    Commit

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
-// 가져올 목록 
+// 가져올 목록  
 const Movie = ({title, poster_path, overview, vote_average }) => (
     <div className="movie">
         <img src={IMG_API + poster_path} alt={title} />
